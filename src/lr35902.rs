6a51db7ed313b27b04f282dcb0fa61f993ac2ff5
MichailTalReborn <mikhailtal60@proton.me>
2026-05-02 12:19:46 +0200

    feat(cpu): Implement ADD and SUB instruction

--- src/lr35902.rs
+++ src/lr35902.rs
@@ -3,7 +3,22 @@ const SUBSTRACT_FLAG_BYTE_POSITION: u8 = 6;
 const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
 const CARRY_FLAG_BYTE_POSITION: u8 = 4;
 
-struct Reagisters {
+enum Instruction {
+    ADD(ArithmeticTarget),
+    SUB(ArithmeticTarget),
+}
+
+enum ArithmeticTarget {
+    A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+struct Registers {
     a: u8,
     b: u8,
     c: u8,
@@ -16,12 +31,12 @@ struct Reagisters {
 
 struct FlagsRegister {
     zero: bool,
-    substract: bool,
+    subtract: bool,
     half_carry: bool,
     carry: bool,
 }
 
-impl Reagisters {
+impl Registers {
     fn get_bc(&self) -> u16 {
         (self.b as u16) << 8 | self.c as u16
     }
@@ -52,12 +67,24 @@ impl Reagisters {
     fn get_hl(&self) -> u16 {
         (self.h as u16) << 8 | self.l as u16
     }
+
+    fn get(&self, target: ArithmeticTarget) -> u8 {
+        match target {
+            ArithmeticTarget::A => self.a,
+            ArithmeticTarget::B => self.b,
+            ArithmeticTarget::C => self.c,
+            ArithmeticTarget::D => self.d,
+            ArithmeticTarget::E => self.e,
+            ArithmeticTarget::H => self.h,
+            ArithmeticTarget::L => self.l,
+        }
+    }
 }
 
 impl std::convert::From<FlagsRegister> for u8 {
     fn from(flag: FlagsRegister) -> u8 {
         (if flag.zero { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION
-            | (if flag.substract { 1 } else { 0 }) << SUBSTRACT_FLAG_BYTE_POSITION
+            | (if flag.subtract { 1 } else { 0 }) << SUBSTRACT_FLAG_BYTE_POSITION
             | (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION
             | (if flag.carry { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
     }
@@ -66,15 +93,51 @@ impl std::convert::From<FlagsRegister> for u8 {
 impl std::convert::From<u8> for FlagsRegister {
     fn from(byte: u8) -> Self {
         let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
-        let substract = ((byte >> SUBSTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBSTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
         let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
         let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
 
         FlagsRegister {
             zero,
-            substract,
+            subtract,
             half_carry,
             carry,
         }
+    }
+}
+
+impl CPU {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADD(target) => {
+                let value = self.registers.get(target);
+                self.registers.a = self.add(value);
+            }
+            Instruction::SUB(target) => {
+                let value = self.registers.get(target);
+                self.registers.a = self.sub(value);
+            }
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = did_overflow;
+        self.register.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+        new_value
+    }
+
+    fn sub(&mut self, value: u8) -> u8 {
+        let a = self.registers.a;
+        let (result, borrow) = a.overflowing_sub(value);
+
+        self.registers.f.zero = result == 0;
+        self.registers.f.subtract = true;
+        self.registers.f.carry = borrow;
+        self.registers.f.half_carry = (a & 0xF) < (value & 0xF);
+
+        result
     }
 }
